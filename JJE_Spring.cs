2ec60b5907e326a2f422caa930d62f7e7c7a6b7e
Jim <tylursims@gmail.com>
2020-11-12 12:25:26 -0500

    Updating
    Adding Library and Spring scripts

---
+++ JJE_Spring.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using UnityEngine;
+
+/// <summary>
+/// The Jim Jam Effects Library
+/// Spring Effect
+/// Move, scale, and rotate (kinda) with a springy twang!
+///
+/// Features:
+/// |- Smooth performant transform manipulations
+/// |- One shot
+/// </summary>
+
+public class JJE_Spring : MonoBehaviour
+{
+    enum SpringOptions // Determine single transform Vector to affect
+    {
+        Mover,
+        Scalar,
+        Rotator_WIP
+    }
+    
+    enum TransformType // By default it's used on transform, but GUI affects rectTransform
+    {
+        WorldSpace,
+        GUI
+    }
+
+    [Header("Spring Settings")]
+    [SerializeField] private SpringOptions effectType = SpringOptions.Mover;
+    [SerializeField] private TransformType transformType = TransformType.WorldSpace;
+    [Range(0.1f,1.0f)]
+    [SerializeField] float springAmount = 0.15f;
+    [Range(0.1f,30.0f)]
+    [SerializeField] float springSpeed = 10.0f;
+    
+    [Header("Effect Settings")]
+    public Vector3 startValue;
+    public Vector3 endValue;
+    public bool isLocal;
+    
+    [Header("Interaction Settings")]
+    [Range(0.1f,3.0f)]
+    [SerializeField] float intervalTime = 1f;
+    
+    // Private Vector variables
+    private Vector3 _target;
+    private Vector3 _value;
+    private Vector3 _valueDir;
+    
+
+    private void Awake()
+    {
+        _target = endValue;
+    }
+
+    private void Update()
+    {
+        // for testing purposes only!
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+            StartSpringLoop();
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+            SpringOneShotToStart();
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+            SpringOneShotToEnd();
+        if(Input.GetKeyDown(KeyCode.Alpha0))
+            StopSpring();
+    }
+
+    public void StartSpringLoop()
+    {
+        StopAllCoroutines();
+        StartCoroutine(DoLoop());
+        IEnumerator DoLoop()
+        {
+            float springTimer = intervalTime;
+            while (springTimer > 0)
+            {
+                //https://answers.unity.com/questions/1111308/unity-coroutine-movement-over-time-is-not-consiste.html
+                // ^- Not 100% sure what it does but it makes it work
+                float elapsedTime = 0;
+                float ratio = elapsedTime / intervalTime;
+                while(ratio < 1f)
+                {
+                    elapsedTime += Time.deltaTime;
+                    ratio = elapsedTime / intervalTime;
+                    Spring();
+                    springTimer -= Time.deltaTime;
+                    yield return null;
+                }
+            }
+            _target = _target == startValue ? endValue : startValue;
+            StartSpringLoop();
+        }
+    }
+
+    public void SpringOneShotToStart()
+    {
+        StopAllCoroutines();
+        _target = startValue;
+        StartCoroutine(SpringOnce());
+    }
+    
+    public void SpringOneShotToEnd()
+    {
+        StopAllCoroutines();
+        _target = endValue;
+        StartCoroutine(SpringOnce());
+    }
+
+    public void StopSpring()
+    {
+        StopAllCoroutines();
+        StartCoroutine(SpringOnce());
+    }
+
+    IEnumerator SpringOnce()
+    {
+        int i = Mathf.RoundToInt(intervalTime);
+        while (i > 0)
+        {
+            float elapsedTime = 0;
+            float ratio = elapsedTime / 1;
+            while(ratio < 1f)
+            {
+                elapsedTime += Time.deltaTime;
+                ratio = elapsedTime / 1;
+                Spring();
+                
+                print("doing one shot");
+                yield return null;
+            } 
+            i--;
+        }
+    }
+
+    void Spring()
+    {
+        _value = Vector3.Lerp(_value, (_target - _valueDir) * springAmount, springSpeed * Time.deltaTime);
+        _valueDir += _value;
+        
+        switch (effectType)
+        {
+            case SpringOptions.Mover:
+                ApplyMove(_valueDir);
+                break;
+            case SpringOptions.Scalar:
+                ApplyScale(_valueDir);
+                break;
+            case SpringOptions.Rotator_WIP:
+                ApplyRotate(_valueDir);
+                break;
+        }
+    }
+
+    void ApplyScale(Vector3 incValue)
+    {
+        switch (transformType)
+        {
+            case TransformType.WorldSpace:
+                this.transform.localScale = incValue;
+                break;
+            case TransformType.GUI:
+                RectTransform rt = GetComponent<RectTransform>();
+                rt.sizeDelta = incValue;
+                break;
+        }
+        
+    }
+
+    void ApplyMove(Vector3 incValue)
+    {
+        switch (transformType)
+        {
+            case TransformType.WorldSpace:
+                if (!isLocal)
+                    this.transform.position = incValue;
+                else this.transform.localPosition = incValue;
+                break;
+            case TransformType.GUI:
+                RectTransform rt = GetComponent<RectTransform>();
+                rt.anchoredPosition = incValue;
+                break;
+        }
+        
+    }
+
+    // Need to find smoother solution
+    void ApplyRotate(Vector3 incValue)
+    {
+        switch (transformType)
+        {
+            case TransformType.WorldSpace:
+                this.transform.rotation = Quaternion.Euler(incValue);
+                break;
+            case TransformType.GUI:
+                RectTransform rt = GetComponent<RectTransform>();
+                rt.rotation = Quaternion.Euler(incValue);
+                break;
+        }
+        
+    }
+    
+    // Possible functions for interacting with the spring
+        // One shot -- go to specific state and stay there
+        // There and back -- go to specific state and return after
+        // Loop -- go to specific state, return after, repeat
+        // Loop for -- go to specific state, return after, repeat until looped x amount of times
+}
---
+++ JimJam_Library.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using static UnityEngine.Application;
+using Debug = UnityEngine.Debug;
+
+/// <summary>
+/// This is a simple task tracking utility for the Unity Engine
+/// https://docs.google.com/document/d/1Ed28y0WwtQDR87IzCQDoIhSVqCMsAqq3OsAjPAo0ASc/edit?usp=sharing
+/// |-> Design Doc
+/// </summary>
+#if UNITY_EDITOR
+
+public class Resource
+{
+    public enum ResourceStates
+    {
+        Same,
+        Newer,
+        Older,
+        NoLocalCopy
+    }
+
+    public ResourceStates state;
+    public string filePath;
+    public string fileName;
+}
+public class JimJam_Library : EditorWindow
+{
+    private static string _libraryPath;
+    private static List<String> _tasks = new List<string>();
+    //private int _textMaxLength = 64;
+    Vector2 _scrollPosition;
+    List<Resource> _resources = new List<Resource>();
+    private bool _updating;
+    [MenuItem("JimJam/Library %g")]
+    public static void ShowWindow()
+    {
+        var window = GetWindow<JimJam_Library>("Jim-Jam Library");
+        window.minSize = new Vector2(380, 425);
+    }
+
+    private void OnGUI()
+    {
+        _scrollPosition = GUILayout.BeginScrollView(
+            _scrollPosition, GUILayout.Height(300));
+        
+        
+        if (GUILayout.Button("Check for Updates", GUILayout.Height(25),GUILayout.Width(175)))
+            CheckForUpdates();
+        if (GUILayout.Button("Open Directory", GUILayout.Height(25), GUILayout.Width(125)))
+            Process.Start(_libraryPath);
+        
+        // Go through each resource and create a section for it
+        if (_resources != null && !_updating)
+            foreach (var file in _resources.ToList())
+            {
+                GUILayout.BeginHorizontal();
+                string displayText = file.fileName;
+                GUILayout.Label(displayText,GUILayout.Height(25));
+                
+                // Condition - The Library version of this resource is the SAME
+                if (file.state == Resource.ResourceStates.Same)
+                {
+                    GUI.contentColor = Color.green;
+                    GUILayout.Label("You have most recent file",GUILayout.Height(25), GUILayout.Width(155));
+                }
+                
+                // Condition - The Library version of this resource is NEWER
+                else if(file.state == Resource.ResourceStates.Newer)
+                {
+                    GUI.contentColor = Color.red;
+                    GUILayout.Label("Needs to be updated",GUILayout.Height(25), GUILayout.Width(155));
+                }
+                
+                // Condition - The Library version of this resource is OLDER 
+                else if(file.state == Resource.ResourceStates.Older)
+                {
+                    GUI.contentColor = Color.yellow;
+                    GUILayout.Label("You have a newer version",GUILayout.Height(25), GUILayout.Width(155));
+                }
+                
+                // Condition - No Local Copy of Library Resource is found
+                else if(file.state == Resource.ResourceStates.NoLocalCopy)
+                {
+                    GUI.contentColor = Color.gray;
+                    GUILayout.Label("No local copy",GUILayout.Height(25), GUILayout.Width(155));
+                }
+                
+                GUI.contentColor = Color.white;
+                if (GUILayout.Button("Push", GUILayout.Height(25), GUILayout.Width(45)))
+                {
+                    PushToLibrary(file.filePath,file.fileName);
+                    continue;
+                }
+
+                if (GUILayout.Button("Get", GUILayout.Height(25), GUILayout.Width(75)))
+                {
+                    PullFromLibrary(file.filePath,file.fileName);
+                    continue;
+                }
+                GUILayout.EndHorizontal();
+            }
+        GUILayout.EndScrollView();
+        if (GUILayout.Button("Backup", GUILayout.Height(25), GUILayout.Width(125)))
+            PushToBackups();
+    }
+
+    void PushToBackups()
+    {
+        var files = Directory.GetFiles(_libraryPath).ToList();
+        foreach (var f in files)
+        {
+            if (f.Contains(".cs"))
+            {
+                File.Copy(f,_libraryPath+"/JimJamLibrary/"+f.Substring(f.LastIndexOf('\\') + 1),true);
+            }
+        }
+    }
+
+    void PushToLibrary(string fp,string fn)
+    {
+        string dataPath = Application.dataPath;
+        var localCopy = Directory.GetFiles(dataPath, fn, SearchOption.AllDirectories);
+        File.Copy(localCopy[0],fp,true);
+        AssetDatabase.Refresh();
+        CheckForUpdates();
+    }
+    
+    void PullFromLibrary(string fp,string fn)
+    {
+        string dataPath = Application.dataPath;
+        var localCopy = Directory.GetFiles(dataPath, fn, SearchOption.AllDirectories);
+        if (localCopy.Length > 0)
+        {
+            File.Copy(fp, localCopy[0],true);
+            //File.Replace(fp, s[0],s[0]);    
+        }
+        else File.Copy(fp, dataPath+"/"+fn);
+        AssetDatabase.Refresh();
+        CheckForUpdates();
+        
+    }
+
+    void CheckForUpdates()
+    {
+        _updating = true;
+        _resources.Clear();
+        var files = Directory.GetFiles(_libraryPath).ToList();
+        foreach (var f in files)
+        {
+            if (f.Contains(".cs"))
+            {
+                var r = new Resource {filePath = f, fileName = f.Substring(f.LastIndexOf('\\') + 1)};
+                r.state = CompareVersions(r.filePath, r.fileName);
+                _resources.Add(r);
+            }
+        }
+        _updating = false;
+    }
+
+    Resource.ResourceStates CompareVersions(string fp,string fn)
+    {
+        string dataPath = Application.dataPath;
+        var localCopy = Directory.GetFiles(dataPath, fn, SearchOption.AllDirectories);
+        if (localCopy.Length > 0)
+        {
+            DateTime lastModified = File.GetLastWriteTime(fp);
+            
+            if(lastModified > File.GetLastWriteTime(localCopy[0]))
+                return Resource.ResourceStates.Newer;
+            if(lastModified == File.GetLastWriteTime(localCopy[0]))
+                return Resource.ResourceStates.Same;
+            
+            return Resource.ResourceStates.Older;    
+        }
+        return Resource.ResourceStates.NoLocalCopy;
+        
+    }
+    
+
+    /* https://stackoverflow.com/questions/20445426/how-to-show-an-ellipses-at-the-end-of-the-text-in-a-textarea
+    public static string TruncateAtWord(string input, int length)
+    {
+        // verify that input is either empty or less than the max character count
+        if (input == null || input.Length < length)
+            return input;
+        int iNextSpace = input.LastIndexOf(" ", length, StringComparison.Ordinal);
+
+        return string.Format("{0}...", input.Substring(0, (iNextSpace > 0) ? iNextSpace : length).Trim());
+    } */
+
+    private void OnValidate()
+    {
+        _libraryPath = Environment.GetFolderPath(
+            Environment.SpecialFolder.ApplicationData) + @"\JimJam\LibraryPackages";
+        if (!Directory.Exists(_libraryPath))
+            Directory.CreateDirectory(_libraryPath);
+        CheckForUpdates();
+    }
+    [MenuItem("Assets/JJL - Send to Library", false, 0)]
+    static void SendFileToLibrary() {
+        var obj = Selection.activeObject;
+        var path = AssetDatabase.GetAssetPath(obj.GetInstanceID());
+        if (path.Length > 0 && path.Contains(".cs"))
+        {
+            Debug.Log(path);
+            string newFileName = path.Substring(7);
+            if (File.Exists(path))
+            {
+                File.Copy(path,_libraryPath+"/"+newFileName,true);
+                AssetDatabase.Refresh();
+            }
+        }
+        else
+        {
+            Debug.LogWarning("JimJamLibrary Warning! " + path + " is not a supported file type");
+        }
+        AssetDatabase.Refresh();
+    }
+}
+
+
+
+#endif
